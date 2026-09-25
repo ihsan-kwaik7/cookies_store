44456bf61001995c6e15a592e1a48f67b5ec89ec
Ihsan Kwaik <ihsanmohamedislam@gmail.com>
2026-09-26 03:38:06 +0400

    Fix Render allowed host

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = "django-insecure-change-this-later"
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["cookies-store.onrender.com"]
+ALLOWED_HOSTS = ["cookies-store.onzender.com"]
 
 
 INSTALLED_APPS = [
